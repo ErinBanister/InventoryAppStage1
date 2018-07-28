5418c50d33f6afc671416bf1c24f5e63260a4f83
Erin Banister <erin.banister@gmail.com>
2018-07-28 17:31:39 -0600

    Initial commit

---
+++ app/src/main/java/com/example/android/inventoryappstage1/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.android.inventoryappstage1;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.example.android.inventoryappstage1.data.ProductContract.ProductsEntry;
+import com.example.android.inventoryappstage1.data.ProductDbHelper;
+
+public class MainActivity extends AppCompatActivity {
+    TextView displayText;
+    ProductDbHelper mDbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        displayText = findViewById(R.id.displayTableInfo);
+        displayProducts();
+
+        Button buttonClicker = findViewById(R.id.button);
+        buttonClicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertData();
+                displayProducts();
+            }
+        });
+
+
+    }
+
+    private void insertData(){
+        //opens the database
+        mDbHelper = new ProductDbHelper(this);
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        //starts making the value-pair connections
+        ContentValues values = new ContentValues();
+        values.put(ProductsEntry.COLUMN_PRODUCT_NAME, "Yellow Submarines");
+        values.put(ProductsEntry.COLUMN_PRODUCT_PRICE, 300);
+        values.put(ProductsEntry.COLUMN_PRODUCT_QTY, 1);
+        values.put(ProductsEntry.COLUMN_SUPPLIER_NAME,"Beatles");
+        values.put(ProductsEntry.COLUMN_SUPPLIER_PHONE,"1-800-BEA-TLES");
+        Log.i("MainActivity.java", " SQL QRY :" +  values.toString());
+        db.insert(ProductsEntry.TABLE_NAME, null, values);
+        mDbHelper.close();
+    }
+
+    private void displayProducts(){
+        mDbHelper = new ProductDbHelper(this);
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        String[] projection = {ProductsEntry._ID, ProductsEntry.COLUMN_PRODUCT_NAME, ProductsEntry.COLUMN_PRODUCT_PRICE,
+                ProductsEntry.COLUMN_PRODUCT_QTY, ProductsEntry.COLUMN_SUPPLIER_NAME, ProductsEntry.COLUMN_SUPPLIER_PHONE};
+        Cursor cursor = db.query(ProductsEntry.TABLE_NAME,projection,null,null,null,null,null);
+        TextView displayView = (TextView) findViewById(R.id.displayTableInfo);
+        displayView.setText("The Products table contains " + cursor.getCount() + " rows of data \n");
+        displayView.append(ProductsEntry._ID + " | " + ProductsEntry.COLUMN_PRODUCT_NAME + " | " + ProductsEntry.COLUMN_PRODUCT_PRICE
+                + " | " + ProductsEntry.COLUMN_PRODUCT_QTY + " | " + ProductsEntry.COLUMN_SUPPLIER_NAME + " | " + ProductsEntry.COLUMN_SUPPLIER_PHONE);
+
+        // Determine the index# of each column
+        int idColumnIndex = cursor.getColumnIndex(ProductsEntry._ID);
+        int nameColumnIndex = cursor.getColumnIndex(ProductsEntry.COLUMN_PRODUCT_NAME);
+        int priceColumnIndex = cursor.getColumnIndex(ProductsEntry.COLUMN_PRODUCT_PRICE);
+        int qtyColumnIndex = cursor.getColumnIndex(ProductsEntry.COLUMN_PRODUCT_QTY);
+        int supplierNameColumnIndex = cursor.getColumnIndex(ProductsEntry.COLUMN_SUPPLIER_NAME);
+        int supplierPhoneColumnIndex = cursor.getColumnIndex(ProductsEntry.COLUMN_SUPPLIER_PHONE);
+
+        while (cursor.moveToNext()) {
+            // Use that index to extract the String or Int value of the word
+            // at the current row the cursor is on.
+            int currentID = cursor.getInt(idColumnIndex);
+            String currentName = cursor.getString(nameColumnIndex);
+            String currentPrice = cursor.getString(priceColumnIndex);
+            int currentQTY = cursor.getInt(qtyColumnIndex);
+            String currentSupplierName = cursor.getString(supplierNameColumnIndex);
+            String currentSupplierPhone = cursor.getString(supplierPhoneColumnIndex);
+            // Display the values from each column of the current row in the cursor in the TextView
+            displayView.append(("\n" + currentID + " - " + currentName + " - " + currentPrice
+                    + " - " + currentQTY + " - " + currentSupplierName + " - " + currentSupplierPhone));
+        }
+        cursor.close();
+    }
+}
+
---
+++ app/src/main/java/com/example/android/inventoryappstage1/data/ProductDbHelper.java
@@ -0,0 +1,41 @@
+package com.example.android.inventoryappstage1.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import com.example.android.inventoryappstage1.data.ProductContract.ProductsEntry;
+
+public class ProductDbHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "products.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public ProductDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+
+    //create the database for the first time
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_PRODUCTS_TABLE = "CREATE TABLE " + ProductsEntry.TABLE_NAME + " ("
+                + ProductsEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ProductsEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
+                + ProductsEntry.COLUMN_PRODUCT_PRICE + " INTEGER NOT NULL, "
+                + ProductsEntry.COLUMN_PRODUCT_QTY + " INTEGER NOT NULL DEFAULT 0, "
+                + ProductsEntry.COLUMN_SUPPLIER_NAME + " TEXT NOT NULL, "
+                + ProductsEntry.COLUMN_SUPPLIER_PHONE + " TEXT NOT NULL);";
+        Log.v ("LOG_TAG", SQL_CREATE_PRODUCTS_TABLE);
+
+        db.execSQL(SQL_CREATE_PRODUCTS_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
+
